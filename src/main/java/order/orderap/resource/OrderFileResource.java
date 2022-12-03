1465ea9318aafa75fe9d7e4b2b7ae1ade2202c3e
meRybaczek <wojciech.rybaczek@gmail.com>
2022-12-03 18:37:08 +0100

    https statuses addes

--- src/main/java/order/orderap/resource/OrderFileResource.java
+++ src/main/java/order/orderap/resource/OrderFileResource.java
@@ -3,6 +3,7 @@ package order.orderap.resource;
 import order.orderap.model.OrderFile;
 import order.orderap.service.OrderFileService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,15 +15,18 @@ public class OrderFileResource {
     private OrderFileService orderFileService;
 
     @PostMapping("/order/{order_id}/orderFile")
+    @ResponseStatus(HttpStatus.CREATED)
     public OrderFile add(@RequestBody OrderFile orderFile, @PathVariable Integer order_id) {
         return orderFileService.addOrderFile(orderFile, order_id);
     }
 
     @GetMapping("/order/{order_id}/orderFile")
+    @ResponseStatus(HttpStatus.FOUND)
     public List<OrderFile> get(@RequestBody OrderFile orderFile, @PathVariable Integer order_id){
         return orderFileService.getByOrderId(order_id);
     }
     @GetMapping("/orderFile/{id}")
+    @ResponseStatus(HttpStatus.FOUND)
     public OrderFile get(@PathVariable Integer id){
         return orderFileService.findById(id);
     }
--- src/main/java/order/orderap/resource/OrderResource.java
+++ src/main/java/order/orderap/resource/OrderResource.java
@@ -3,6 +3,7 @@ package order.orderap.resource;
 import order.orderap.model.OrderPdf;
 import order.orderap.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.List;
@@ -15,6 +16,7 @@ public class OrderResource {
     private OrderService orderService;
 
     @PostMapping("/order")
+    @ResponseStatus(HttpStatus.CREATED)
     public OrderPdf add(@RequestBody OrderPdf order) {
         return orderService.addOrder(order);
     }
@@ -25,6 +27,7 @@ public class OrderResource {
     }
 
     @GetMapping("/order")
+    @ResponseStatus(HttpStatus.FOUND)
     public List<OrderPdf> getAllOrders(@RequestParam(required = false) String clientName,
                                        @RequestParam(required = false) String clientPhone,
                                        @RequestParam(required = false) LocalDate createdAt,
@@ -33,6 +36,7 @@ public class OrderResource {
 
         }
     @GetMapping("/order/{id}")
+    @ResponseStatus(HttpStatus.FOUND)
     public OrderPdf findById(@PathVariable Integer id) {
         return orderService.findById(id);
 
