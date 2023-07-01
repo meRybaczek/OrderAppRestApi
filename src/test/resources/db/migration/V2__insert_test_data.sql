4af73ee24221970291d8573490a2899d70eac5dc
meRybaczek <wojciech.rybaczek@gmail.com>
2023-07-01 14:36:30 +0200

    current date set in test sql flyway query V2__

--- src/test/resources/db/migration/V2__insert_test_data.sql
+++ src/test/resources/db/migration/V2__insert_test_data.sql
@@ -1,3 +1,3 @@
 INSERT INTO client VALUES(1,'archi@archi.com', 'Archi1',5.0, '779-232-84-28');
-INSERT INTO order_pdf VALUES(1, '2024-12-12',1);
+INSERT INTO order_pdf VALUES(1, CURRENT_DATE,1);
 INSERT INTO order_file VALUES(1,3, 297.0, 594.0, 'C:\test','Rys1','true','true',1);
